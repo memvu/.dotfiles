aa33aaa5dffc123e8e67046890e7d191fe9806eb
memvu <hoang040507@gmail.com>
2026-07-23 15:49:20 +0700

    [REF] deprecated lsp setup

--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -52,14 +52,10 @@ return {
             'texlab',
             'tinymist',
             -- 'cmake',
-            -- 'neocmakelsp',
+            -- 'neocmake',
           },
-          automatic_enable = {
-            exclude = {
-              'jdtls',
-            },
-          },
-          automatic_installation = true,
+          -- Server configurations are registered and enabled explicitly below.
+          automatic_enable = false,
         },
       },
       {
@@ -232,34 +228,31 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 
-      -- multithreading for clangd
+      -- Build clangd's process arguments separately from compiler arguments.
       local nproc = tonumber(vim.fn.system { 'sysctl', '-n', 'hw.logicalcpu' })
-      local opt_jnproc = ''
+      local clangd_cmd = {
+        'clangd',
+        '--background-index',
+        '--header-insertion=iwyu',
+      }
       if nproc and nproc > 1 then
-        opt_jnproc = '-j' .. tostring(nproc - 1)
+        vim.list_extend(clangd_cmd, { '-j', tostring(nproc - 1) })
       end
 
       local servers = {
-        clangd = {
-          cmd = {
-            'clangd',
-            '--background-index',
-            '--header-insertion=iwyu',
-            opt_jnproc,
-          },
-        },
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        -- jdtls = {},
-        neocmakelsp = {},
+        clangd = { cmd = clangd_cmd },
+        pyright = {},
+        taplo = {},
+        html = {},
+        ts_ls = {},
+        cssls = {},
+        jsonls = {},
+        eslint = {},
+        tailwindcss = {},
+        omnisharp = {},
+        tinymist = {},
+        dockerls = {},
+        neocmake = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -306,20 +299,7 @@ return {
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
+      local ensure_installed = vim.tbl_keys(servers)
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'prettier',
@@ -329,19 +309,14 @@ return {
         'eslint_d',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-            -- vim.lsp.config[server_name];
-          end,
-        },
-      }
+
+      -- Apply shared capabilities and per-server overrides through Neovim's
+      -- native LSP API, then enable the complete configured server set.
+      vim.lsp.config('*', { capabilities = capabilities })
+      for server_name, server in pairs(servers) do
+        vim.lsp.config(server_name, server)
+      end
+      vim.lsp.enable(vim.tbl_keys(servers))
     end,
   },
 }
